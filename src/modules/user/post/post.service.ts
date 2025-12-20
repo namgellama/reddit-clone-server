b20d3cab90ecf30a41d5b9c052402dcacb7f9b35
Namgel <namgeltamang14@gmail.com>
2025-12-20 19:20:55 +0545

    Include comments and upvotes in post related response

--- src/modules/user/post/post.service.ts
+++ src/modules/user/post/post.service.ts
@@ -18,17 +18,63 @@ const postService = {
 
     // Get all
     getAll: async () => {
-        return await prisma.post.findMany();
+        const posts = await prisma.post.findMany({
+            include: {
+                _count: {
+                    select: {
+                        comments: true,
+                        upvotes: true,
+                    },
+                },
+            },
+        });
+
+        return posts.map(({ _count, ...rest }) => ({
+            ...rest,
+            commentsCount: _count.comments,
+            upvotesCount: _count.upvotes,
+        }));
     },
 
     // Get by id
     getById: async (id: string) => {
-        const existingPost = await prisma.post.findUnique({ where: { id } });
+        const existingPost = await prisma.post.findUnique({
+            where: { id },
+            include: {
+                comments: {
+                    include: {
+                        _count: {
+                            select: {
+                                replies: true,
+                                upvotes: true,
+                            },
+                        },
+                    },
+                },
+                _count: {
+                    select: {
+                        comments: true,
+                        upvotes: true,
+                    },
+                },
+            },
+        });
 
         if (!existingPost)
             throw apiError(StatusCodes.NOT_FOUND, 'Post not found');
 
-        return existingPost;
+        const { _count, comments, ...rest } = existingPost;
+
+        return {
+            ...rest,
+            comments: comments.map(({ _count, ...rest }) => ({
+                ...rest,
+                repliesCount: _count.replies,
+                upvotesCount: _count.upvotes,
+            })),
+            commentsCount: _count.comments,
+            upvotesCount: _count.upvotes,
+        };
     },
 
     // Update
