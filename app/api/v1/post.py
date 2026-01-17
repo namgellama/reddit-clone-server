d735dcef8b68a48f544bcc620e4657625e56df87
Namgel <namgeltamang14@gmail.com>
2026-01-17 14:40:27 +0545

    Build api for fetching post by id

--- app/api/v1/post.py
+++ app/api/v1/post.py
@@ -1,4 +1,5 @@
 from typing import Annotated
+from uuid import UUID
 
 from fastapi import APIRouter, Depends
 
@@ -15,3 +16,8 @@ router = APIRouter()
 @router.get("/", response_model=list[PostResponse])
 def get_posts(db: Annotated[Session, Depends(get_db)]):
     return post.get_all(db)
+
+
+@router.get("/{id}", response_model=PostResponse)
+def get_post(id: UUID, db: Annotated[Session, Depends(get_db)]):
+    return post.get_by_id(id=id, db=db)
--- app/services/post.py
+++ app/services/post.py
@@ -1,6 +1,7 @@
 from typing import Annotated
+from uuid import UUID
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
 
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -13,3 +14,14 @@ def get_all(db: Annotated[Session, Depends(get_db)]):
     result = db.execute(select(Post))
     posts = result.scalars().all()
     return posts
+
+
+def get_by_id(id: UUID, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(Post).where(Post.id == id))
+    post = result.scalars().first()
+
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post does not exist")
+
+    return post
