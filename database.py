8557405520584897490438ad26a330e0316bb07b
Namgel <namgeltamang14@gmail.com>
2026-01-17 10:48:40 +0545

    Configure database

---
+++ database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from config import DATABASE_URL
+
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+    with SessionLocal() as db:
+        yield db
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 
+from database import Base, engine, get_db
+
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 
