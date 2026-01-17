faae830c445f5e3bef7d41843ac009a787870906
Namgel <namgeltamang14@gmail.com>
2026-01-17 11:03:48 +0545

    Create post model

---
+++ models.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from uuid import uuid4
+from datetime import UTC, datetime
+
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import DateTime, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from database import Base
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid4, index=True)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    date_posted: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), default=lambda: datetime.now(UTC))
