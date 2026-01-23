2209d79a0e9edde4a7e5b7e7c0ec2644401df849
Namgel <namgeltamang14@gmail.com>
2026-01-23 10:40:57 +0545

    Create otp utility functions

---
+++ app/utils/otp.py
@@ -0,0 +1,19 @@
+
+import random
+
+from fastapi import Request
+
+from app.services.redis import redis_client
+
+
+def generate_otp():
+    return str(random.randint(100000, 999999))
+
+
+async def store_otp(name: str, time: int, value: str):
+    await redis_client.setex(name, time, value)
+
+
+async def verify_otp(name: str, otp: str):
+    stored_otp = await redis_client.get(name)
+    return stored_otp == otp
