ae8675216c2da9035d2c211ac762c1461386cda5
Namgel <namgeltamang14@gmail.com>
2026-01-23 11:16:37 +0545

    Configure mail service

---
+++ app/config/mail.py
@@ -0,0 +1,14 @@
+from fastapi_mail import ConnectionConfig
+
+from app.config import env
+
+conf = ConnectionConfig(
+    MAIL_USERNAME=env.MAIL_USERNAME,
+    MAIL_PASSWORD=env.MAIL_PASSWORD,
+    MAIL_FROM=env.MAIL_USERNAME,
+    MAIL_PORT=587,
+    MAIL_SERVER="smtp.gmail.com",
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
+    USE_CREDENTIALS=True,
+)
---
+++ app/services/mail.py
@@ -0,0 +1,18 @@
+from pydantic import EmailStr
+
+from fastapi_mail import FastMail, MessageSchema
+
+from app.config.mail import conf
+
+
+async def send_mail(subject: str, recipients: list[EmailStr], body: str):
+    message = MessageSchema(
+        subject=subject,
+        recipients=recipients,
+        body=body,
+        subtype="html"
+    )
+
+    fm = FastMail(conf)
+
+    await fm.send_message(message)
