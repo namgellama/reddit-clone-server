fe8901d037d33cea2e8c2acc521787f70e1b8d60
Namgel <namgeltamang14@gmail.com>
2026-01-24 10:41:56 +0545

    Configure google oauth

--- app/config/env.py
+++ app/config/env.py
@@ -3,14 +3,26 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Environment
 ENV = os.getenv("ENV")
+FRONTEND_URL = os.getenv("FRONTEND_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
+# Database
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# JWT
 JWT_ACCESS_SECRET = os.getenv("JWT_ACCESS_SECRET")
 JWT_REFRESH_SECRET = os.getenv("JWT_REFRESH_SECRET")
 
+# Redis
 REDIS_URL = os.getenv("REDIS_URL")
 
+# Mail
 MAIL_USERNAME = os.getenv("MAIL_USERNAME")
 MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+
+# GOOGLE Oauth
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
