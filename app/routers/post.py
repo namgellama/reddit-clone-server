83ca0f8ccbeb7592a3f15ab3da205d5b32854cd3
Namgel <namgeltamang14@gmail.com>
2026-01-31 20:13:20 +0545

    Refactor post apis

--- app/routers/post.py
+++ app/routers/post.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.config.database import get_db
-from app.schemas.post import PostResponse, PostCreate, PostBase, PostDetailsResponse
+from app.schemas.post import PostResponse, PostCreate, PostBase, PostDetailsResponse, PostUpdate
 from app.schemas.user import UserResponse
 from app.services import post as post_service
 from app.services.user import get_current_user
@@ -12,6 +12,13 @@ from app.schemas.response import APIResponse
 
 
 router = APIRouter()
+
+"""
+    @desc Get all posts
+    @route GET /api/v1/posts
+    @access Public
+
+"""
 
 
 @router.get("/", response_model=APIResponse[list[PostResponse]])
@@ -21,11 +28,26 @@ async def get_posts(db: Annotated[AsyncSession, Depends(get_db)]):
     return APIResponse(success=True, message="Posts fetched successfully", data=posts)
 
 
+"""
+    @desc Get a post
+    @route GET /api/v1/posts/:id
+    @access Public
+
+"""
+
+
 @router.get("/{id}", response_model=APIResponse[PostDetailsResponse])
 async def get_post(id: UUID, db: Annotated[AsyncSession, Depends(get_db)]):
     post = await post_service.get_by_id(id=id, db=db)
 
     return APIResponse(success=True, message="Post fetched successfully", data=PostDetailsResponse.model_validate(post))
+
+"""
+    @desc Create a post
+    @route POST /api/v1/posts
+    @access Private
+
+"""
 
 
 @router.post("/", response_model=APIResponse[PostResponse], status_code=status.HTTP_201_CREATED)
@@ -36,16 +58,32 @@ async def create_post(body: PostBase, current_user: Annotated[UserResponse, Depe
     return APIResponse(success=True, message="Post created successfully", data=new_post)
 
 
+"""
+    @desc Update a post
+    @route PUT /api/v1/posts
+    @access Private
+
+"""
+
+
 @router.put("/{id}", response_model=APIResponse[PostResponse])
 async def update_post(id: UUID, body: PostBase, current_user: Annotated[UserResponse, Depends(get_current_user)], db: Annotated[AsyncSession, Depends(get_db)]):
-    payload = PostCreate(**body.model_dump(), user_id=current_user.id)
-    updated_post = await post_service.update(id=id, payload=payload, db=db)
+    payload = PostUpdate(**body.model_dump(), user_id=current_user.id, id=id)
+    updated_post = await post_service.update(payload=payload, db=db)
 
     return APIResponse(success=True, message="Post updated successfully", data=updated_post)
 
 
+"""
+    @desc Delete a post
+    @route DELETE /api/v1/posts/:id
+    @access Private
+
+"""
+
+
 @router.delete("/{id}", response_model=APIResponse[None])
-async def delete_post(id: UUID,  db: Annotated[AsyncSession, Depends(get_db)]):
-    await post_service.delete(id=id, db=db)
+async def delete_post(id: UUID, current_user: Annotated[UserResponse, Depends(get_current_user)], db: Annotated[AsyncSession, Depends(get_db)]):
+    await post_service.delete(id=id, user_id=current_user.id, db=db)
 
     return APIResponse(success=True, message="Post deleted successfully", data=None)
--- app/services/post.py
+++ app/services/post.py
@@ -1,22 +1,24 @@
-from typing import Annotated
 from uuid import UUID
-from fastapi import Depends, HTTPException, status
+from fastapi import HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.orm import selectinload
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.config.database import get_db
 from app.models.post import Post
-from app.schemas.post import PostCreate
+from app.schemas.post import PostCreate, PostUpdate
+
+# Get all
 
 
-async def get_all(db: Annotated[AsyncSession, Depends(get_db)]):
+async def get_all(db: AsyncSession):
     result = await db.execute(select(Post).options(selectinload(Post.author)))
     posts = result.scalars().all()
     return posts
 
+# Get by id
 
-async def get_by_id(id: UUID, db: Annotated[AsyncSession, Depends(get_db)]):
+
+async def get_by_id(id: UUID, db: AsyncSession):
     result = await db.execute(select(Post).options(selectinload(Post.author)).where(Post.id == id))
     post = result.scalars().first()
 
@@ -26,8 +28,10 @@ async def get_by_id(id: UUID, db: Annotated[AsyncSession, Depends(get_db)]):
 
     return post
 
+# Create
 
-async def create(payload: PostCreate, db: Annotated[AsyncSession, Depends(get_db)]):
+
+async def create(payload: PostCreate, db: AsyncSession):
     new_post = Post(title=payload.title,
                     content=payload.content, user_id=payload.user_id)
 
@@ -36,9 +40,11 @@ async def create(payload: PostCreate, db: Annotated[AsyncSession, Depends(get_db
     await db.refresh(new_post, attribute_names=['author'])
     return new_post
 
+# Update
 
-async def update(id: UUID, payload: PostCreate, db: Annotated[AsyncSession, Depends(get_db)]):
-    result = await db.execute(select(Post).where(Post.id == id))
+
+async def update(payload: PostUpdate, db: AsyncSession):
+    result = await db.execute(select(Post).where(Post.id == payload.id))
     post = result.scalars().first()
 
     if not post:
@@ -47,7 +53,7 @@ async def update(id: UUID, payload: PostCreate, db: Annotated[AsyncSession, Depe
 
     if post.user_id != payload.user_id:
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to update this post")
+            status_code=status.HTTP_403_FORBIDDEN, detail="Not allowed")
 
     post.title = payload.title
     post.content = payload.content
@@ -57,14 +63,20 @@ async def update(id: UUID, payload: PostCreate, db: Annotated[AsyncSession, Depe
     await db.refresh(post, attribute_names=['author'])
     return post
 
+# Delete
 
-async def delete(id: UUID, db: Annotated[AsyncSession, Depends(get_db)]):
+
+async def delete(id: UUID, user_id: UUID,  db: AsyncSession):
     result = await db.execute(select(Post).where(Post.id == id))
     post = result.scalars().first()
 
     if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Post does not exist")
+
+    if post.user_id != user_id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Not allowed")
 
     await db.delete(post)
     await db.commit()
